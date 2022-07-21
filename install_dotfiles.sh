a0dc8988ed4a497cb321a67471f8e92503714cc4
tkutschbach <tino.kutschbach@mailbox.org>
2022-07-21 13:08:18 +0200

    added .Xresources file

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,3 +3,6 @@ git clone --bare https://github.com/tkutschbach/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
 config config --local status.showUntrackedFiles no
+
+#initialize Xresources
+xrdb -merge .Xresources
