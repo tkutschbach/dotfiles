47b1ae816cf54e1ab85bbadc7bf2888aca709fdd
tkutschbach <tino.kutschbach@mailbox.org>
2022-07-27 14:40:21 +0200

    Replaced bash alias by function inside setup and update scripts

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,7 +5,11 @@ fi
 
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/tkutschbach/dotfiles.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+function config()
+{
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME "$@"
+}
 config checkout
 config config --local status.showUntrackedFiles no
 
--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,3 +1,9 @@
+
+function config()
+{
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME "$@"
+}
+
 # pull new version from repository
 config pull
 # reinit Xresources
